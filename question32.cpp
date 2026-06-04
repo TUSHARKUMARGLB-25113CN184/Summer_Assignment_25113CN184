ae45312dc9dabce636441fdabdf7c7a414ea578c
TUSHARKUMARGLB-25113CN184 <tk979293@gmail.com>
2026-06-04 16:26:30 +0530

    DAY-8

---
+++ question32.cpp
@@ -0,0 +1,25 @@
+/*Write a program to Print repeated-number 
+pattern. 
+1 
+22 
+333 
+4444 
+55555*/
+#include<iostream>
+  using namespace std;
+int main()
+{
+    int rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1; j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<"\n";
+        
+    }
+return 0;
+}
