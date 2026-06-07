e7043f3e27ab2624d41c4410e863317a235ff9cc
TUSHARKUMARGLB-25113CN184 <tk979293@gmail.com>
2026-06-07 21:26:42 +0530

    DAY-11

---
+++ question41.c
@@ -0,0 +1,13 @@
+/*Write a program to Write function to find sum 
+of two numbers */
+#include<stdio.h>
+int sum(int a,int b){
+    return a+b;
+}
+int main(){
+    int num1,num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+    printf("Sum of %d and %d is %d",num1,num2,sum(num1,num2));
+    return 0;
+}
---
+++ question42.c
@@ -0,0 +1,16 @@
+/*Write a program to Write function to find 
+maximum*/
+#include<stdio.h>
+int max(int a,int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}
+int main(){
+    int num1,num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1,&num2);
+    printf("Maximum of %d and %d is %d",num1,num2,max(num1,num2));
+    return 0;
+}
